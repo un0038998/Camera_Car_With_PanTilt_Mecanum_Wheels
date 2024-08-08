78db391c1340b97dc48f0dae9a665eaad6d73376
Ujwal Nandanwar <75965943+un0038998@users.noreply.github.com>
2024-08-08 16:37:56 +0530

    Initial files for Camera_Car_With_PanTilt_Mecanum_Wheels

---
+++ Camera_Car_With_PanTilt_Mecanum_Wheels_ESP32/Camera_Car_With_PanTilt_Mecanum_Wheels_ESP32.ino
@@ -0,0 +1,234 @@
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <ESP32Servo.h>
+
+#define PAN_PIN 12
+#define TILT_PIN 13
+ 
+Servo panServo;
+Servo tiltServo;
+
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+#define UP_LEFT 5
+#define UP_RIGHT 6
+#define DOWN_LEFT 7
+#define DOWN_RIGHT 8
+#define TURN_LEFT 9
+#define TURN_RIGHT 10
+#define STOP 0
+
+#define FRONT_RIGHT_MOTOR 0
+#define BACK_RIGHT_MOTOR 1
+#define FRONT_LEFT_MOTOR 2
+#define BACK_LEFT_MOTOR 3
+
+#define FORWARD 1
+#define BACKWARD -1
+
+struct MOTOR_PINS
+{
+  int pinIN1;
+  int pinIN2;
+  int pinEn; 
+  int pwmSpeedChannel;
+};
+
+std::vector<MOTOR_PINS> motorPins = 
+{
+  {16, 17, 22, 4},  //FRONT_RIGHT_MOTOR
+  {18, 19, 23, 5},  //BACK_RIGHT_MOTOR
+  {26, 25, 32, 6},  //FRONT_LEFT_MOTOR
+  {14, 27, 33, 7},  //BACK_LEFT_MOTOR   
+};
+
+const int PWMFreq = 1000; /* 1 KHz */
+const int PWMResolution = 8;
+
+#define SIGNAL_TIMEOUT 1000  // This is signal timeout in milli seconds. We will reset the data if no signal
+unsigned long lastRecvTime = 0;
+
+void processCarMovement(int inputValue)
+{
+  switch(inputValue)
+  {
+
+    case UP:
+      rotateMotor(FRONT_RIGHT_MOTOR, FORWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, FORWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, FORWARD);
+      rotateMotor(BACK_LEFT_MOTOR, FORWARD);                  
+      break;
+  
+    case DOWN:
+      rotateMotor(FRONT_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, BACKWARD);
+      rotateMotor(BACK_LEFT_MOTOR, BACKWARD);   
+      break;
+  
+    case LEFT:
+      rotateMotor(FRONT_RIGHT_MOTOR, FORWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, BACKWARD);
+      rotateMotor(BACK_LEFT_MOTOR, FORWARD);   
+      break;
+  
+    case RIGHT:
+      rotateMotor(FRONT_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, FORWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, FORWARD);
+      rotateMotor(BACK_LEFT_MOTOR, BACKWARD);  
+      break;
+  
+    case UP_LEFT:
+      rotateMotor(FRONT_RIGHT_MOTOR, FORWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, STOP);
+      rotateMotor(FRONT_LEFT_MOTOR, STOP);
+      rotateMotor(BACK_LEFT_MOTOR, FORWARD);  
+      break;
+  
+    case UP_RIGHT:
+      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
+      rotateMotor(BACK_RIGHT_MOTOR, FORWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, FORWARD);
+      rotateMotor(BACK_LEFT_MOTOR, STOP);  
+      break;
+  
+    case DOWN_LEFT:
+      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
+      rotateMotor(BACK_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, BACKWARD);
+      rotateMotor(BACK_LEFT_MOTOR, STOP);   
+      break;
+  
+    case DOWN_RIGHT:
+      rotateMotor(FRONT_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, STOP);
+      rotateMotor(FRONT_LEFT_MOTOR, STOP);
+      rotateMotor(BACK_LEFT_MOTOR, BACKWARD);   
+      break;
+  
+    case TURN_LEFT:
+      rotateMotor(FRONT_RIGHT_MOTOR, FORWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, FORWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, BACKWARD);
+      rotateMotor(BACK_LEFT_MOTOR, BACKWARD);  
+      break;
+  
+    case TURN_RIGHT:
+      rotateMotor(FRONT_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(BACK_RIGHT_MOTOR, BACKWARD);
+      rotateMotor(FRONT_LEFT_MOTOR, FORWARD);
+      rotateMotor(BACK_LEFT_MOTOR, FORWARD);   
+      break;
+  
+    case STOP:
+      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
+      rotateMotor(BACK_RIGHT_MOTOR, STOP);
+      rotateMotor(FRONT_LEFT_MOTOR, STOP);
+      rotateMotor(BACK_LEFT_MOTOR, STOP);    
+      break;
+  
+    default:
+      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
+      rotateMotor(BACK_RIGHT_MOTOR, STOP);
+      rotateMotor(FRONT_LEFT_MOTOR, STOP);
+      rotateMotor(BACK_LEFT_MOTOR, STOP);    
+      break;
+  }
+}
+
+void rotateMotor(int motorNumber, int motorDirection)
+{
+  if (motorDirection == FORWARD)
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, HIGH);
+    digitalWrite(motorPins[motorNumber].pinIN2, LOW);    
+  }
+  else if (motorDirection == BACKWARD)
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
+    digitalWrite(motorPins[motorNumber].pinIN2, HIGH);     
+  }
+  else
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
+    digitalWrite(motorPins[motorNumber].pinIN2, LOW);       
+  }
+}
+
+void setUpPinModes()
+{
+  panServo.attach(PAN_PIN);
+  tiltServo.attach(TILT_PIN);
+    
+  for (int i = 0; i < motorPins.size(); i++)
+  {
+    pinMode(motorPins[i].pinIN1, OUTPUT);
+    pinMode(motorPins[i].pinIN2, OUTPUT);  
+    //Set up PWM for motor speed
+    ledcSetup(motorPins[i].pwmSpeedChannel, PWMFreq, PWMResolution);  
+    ledcAttachPin(motorPins[i].pinEn, motorPins[i].pwmSpeedChannel);     
+    rotateMotor(i, STOP);  
+  }
+}
+
+void setup() 
+{
+  setUpPinModes();
+  Serial1.begin(115200, SERIAL_8N1, 4, 15);
+  Serial.begin(115200);
+}
+
+void loop() 
+{
+  String dataString;
+  char key[500];
+  int value;
+  if (Serial1.available())
+  {
+    dataString = Serial1.readStringUntil('\n');
+
+    std::string myData = dataString.c_str();
+    std::istringstream ss(myData);
+    std::string key, value;
+    std::getline(ss, key, ',');
+    std::getline(ss, value, ',');
+    Serial.printf("Key [%s] Value[%s]\n", key.c_str(), value.c_str()); 
+    int valueInt = atoi(value.c_str()); 
+
+    if (key == "MoveCar")
+    {
+      processCarMovement(valueInt);
+    }
+    else if (key == "Speed")
+    {
+      for (int i = 0; i < motorPins.size(); i++)
+      {
+        ledcWrite(motorPins[i].pwmSpeedChannel, valueInt);     
+      }
+    }
+    else if (key == "Pan")
+    {
+      panServo.write(valueInt);
+    }
+    else if (key == "Tilt")
+    {
+      tiltServo.write(valueInt);   
+    }     
+
+    lastRecvTime = millis();   
+  }
+
+}
+
+
+
+
+
+
+
